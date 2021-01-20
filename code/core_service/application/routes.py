e1bc4e742b768033d549e30d930e4ee751f8ac19
NaailC <naailchoudhury@gmail.com>
2021-01-20 10:45:13 +0000

    backup

--- code/core_service/application/routes.py
+++ code/core_service/application/routes.py
@@ -6,10 +6,10 @@ from requests import get
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    weapon = requests.get('http://buy_backend:5001/get/buy')
-    strat = requests.get('http://strat_backend:5002/get/strat')
-    roundstrength = requests.post('http://round_strength:5003/post')
-    return render_template('home.html', weapon=weapon, strat=strat)
+    weapon_response = requests.get('http://CSGO-code-buy_backend:5001/get/buy')
+    strat_response = requests.get('http://CSGO-code-strat_backend:5002/get/strat')
+    roundstrength_response = requests.post('http://CSGO-code-round_strength:5003/post', json={'buystrength' : weapon_response.json['x'], 'stratstrength' : strat_response.json['strat']}
+    return render_template('home.html', weapon=weapon_response.text, strat=strat_response.text, roundstrength=roundstrength_response.text)
 
 
 
