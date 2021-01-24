7a7b0695dd0359493714e724563505405b69c490
naailc <naailchoudhury@gmail.com>
2021-01-24 16:20:51 +0000

    pipeline test

--- code/core_service/tests/test_app.py
+++ code/core_service/tests/test_app.py
@@ -3,7 +3,7 @@ from unittest.mock import patch
 from flask_testing import TestCase
 import requests_mock
 
-from application import app, db
+from application import app, db, routes
 from application.routes import Buy
 
 class TestBase(TestCase):
@@ -25,21 +25,21 @@ class TestResponse(TestBase):
     def test_AK(self):
         with requests_mock.mock() as m:
             m.get('http://CSGOBUY-buy_backend:5001/get/buy', text='AK')
-            m.post('http://CSGOBUY-buy_backend:5001/post/buystrength', text='100') 
+            m.post('http://CSGOBUY-buy_backend:5001/post/buystrength', text='100.0') 
             m.get('http://CSGOBUY-strat_backend:5002/get/strat', text='Rush A')
-            m.post('http://CSGOBUY-strat_backend:5002/post/stratstrength', text='50')
-            m.post('http://CSGOBUY-round_strength:5003/post/roundstrength', text='75')
+            m.post('http://CSGOBUY-strat_backend:5002/post/stratstrength', text='50.0')
+            m.post('http://CSGOBUY-round_strength:5003/post/roundstrength', text='75.0')
             response = self.client.get(url_for('home'))
-            self.assertIn(b'Buy AK and Rush A', response.data)
+            self.assertIn(b'AK and Rush A - power-level 75.0', response.data)
             self.assertIn(b'testweapon and teststrat - power-level testp', response.data)
 
     def test_P90(self):
         with requests_mock.mock() as m:
             m.get('http://CSGOBUY-buy_backend:5001/get/buy', text='p90')
-            m.post('http://CSGOBUY-buy_backend:5001/post/buystrength', text='60') 
+            m.post('http://CSGOBUY-buy_backend:5001/post/buystrength', text='60.0') 
             m.get('http://CSGOBUY-strat_backend:5002/get/strat', text='Rush B')
-            m.post('http://CSGOBUY-strat_backend:5002/post/stratstrength', text='60')
-            m.post('http://CSGOBUY-round_strength:5003/post/roundstrength', text='60')
+            m.post('http://CSGOBUY-strat_backend:5002/post/stratstrength', text='60.0')
+            m.post('http://CSGOBUY-round_strength:5003/post/roundstrength', text='60.0')
             response = self.client.get(url_for('home'))
-            self.assertIn(b'Buy p90 and Rush B', response.data)
+            self.assertIn(b'p90 and Rush B - power-level 60.0', response.data)
             self.assertIn(b'testweapon and teststrat - power-level testp', response.data)
