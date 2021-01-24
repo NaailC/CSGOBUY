2c284cc5e0a125c9f766bbe4bab2d066513bb2af
naailc <naailchoudhury@gmail.com>
2021-01-24 21:02:19 +0000

    jenkins push

--- jenkins/script/deploy.sh
+++ jenkins/script/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-scp -i ~/jenkins_agent_key docker-compose.yaml jenkins@swarm-master:/home/jenkins/docker-compose.yaml
-ssh -i ~/jenkins_agent_key jenkins@swarm-master << EOF
+scp -i ~/.ssh/id_rsa docker-compose.yaml jenkins@swarm-master:/home/jenkins/docker-compose.yaml
+ssh -i ~/.ssh/id_rsa jenkins@swarm-master << EOF
     export DATABASE_URI=${DATABASE_URI} replicas=${replicas}
     docker stack deploy --compose-file /home/jenkins/docker-compose.yaml CSGOBG-stack
 EOF
